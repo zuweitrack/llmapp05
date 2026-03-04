2bf702a3db6136195081814ba5059e9f0f1cf3b0
Zu Wei <zuweitrack@gmail.com>
2026-03-04 14:39:21 +0800

    Update Docker Hub username to own account

--- llm-frontend-python/build.sh
+++ llm-frontend-python/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t darryl1975/llm-frontend-python:latest .        
-  docker push darryl1975/llm-frontend-python:latest 
+  docker build --platform linux/arm64 -t zuweitrack/llm-frontend-python:latest .        
+  docker push zuweitrack/llm-frontend-python:latest 
--- llm-multiroute/build.sh
+++ llm-multiroute/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t darryl1975/llm-multiroute:latest .        
-  docker push darryl1975/llm-multiroute:latest 
+  docker build --platform linux/arm64 -t zuweitrack/llm-multiroute:latest .        
+  docker push zuweitrack/llm-multiroute:latest 
